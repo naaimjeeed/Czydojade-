27fc2ae1ea2a73bf1adb3c9a274441937ddca654
naaimjeeed <jiegieskier@gmail.com>
2026-09-17 10:40:40 +0200

    Add Stop type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, World!");
+import type { Stop } from "./types/Stop";
+// stop to tylko typ, dlatego dodajemy type 
+
+const appName: string = "Czydojadę?";
+console.log(`Welcome to ${appName}!`);
+
+const stop: Stop = {
+    id: "01",
+    name: "Dw. Centralny 01",
+    latitude: 52.2296756,
+    longitude: 21.0122287
+};
+
+console.log(stop);
---
+++ src/types/Stop.ts
@@ -0,0 +1,8 @@
+export type Stop = { //type - opisuje strukturę obiektu
+    id: string;  //w tym przypadku reprezentuje przystanek
+    name: string;
+    latitude: number;
+    longitude: number;
+};
+
+
